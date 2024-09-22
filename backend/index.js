3d5ab9f65ac7335a0c236029bb0bfb787fb5d964
nguyentrunghieu297 <trunghieunguyen2729@gmail.com>
2024-09-22 14:28:28 +0700

    .

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Kết nối database
-mongoose.connect('mongodb://localhost:27017/yourdb', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/pawfund', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log('Error connecting to MongoDB:', err));
 // Routes
