b39fc30dc7ae1b9a0dc09e7bae5fb23325003892
nguyentrunghieu297 <trunghieunguyen2729@gmail.com>
2024-09-23 14:15:59 +0700

    Minor fix

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 
 const userRoutes = require('./routes/authRoute');
 const petRoutes = require('./routes/petsRoute');
@@ -8,16 +12,29 @@ const shelterRoutes = require('./routes/shelterRoutes');
 const adoptionRoutes = require('./routes/adoptionsRoute');
 const donationRoutes = require('./routes/donationsRoute');
 const eventRoutes = require('./routes/eventsRoute');
+const { connect } = require('./config/database');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Config
+dotenv.config();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(methodOverride('_method'));
+app.use(cookieParser());
+
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Http logger
+app.use(morgan('combined'));
+
 // Kết nối database
-mongoose.connect('mongodb://localhost:27017/pawfund')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log('Error connecting to MongoDB:', err));
+connect();
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/pets', petRoutes);
@@ -26,4 +43,6 @@ app.use('/api/adoptions', adoptionRoutes);
 app.use('/api/donations', donationRoutes);
 app.use('/api/events', eventRoutes);
 
-module.exports = app;
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port http://localhost:${process.env.PORT}`);
+});
