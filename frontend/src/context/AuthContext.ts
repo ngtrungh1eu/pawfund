54c77312fe14dda75535235984ba5f9f0660a447
Phước <tranlehuuphuoc22502@gmail.com>
2024-10-30 23:24:38 +0700

    fix-auth-context

--- frontend/src/context/AuthContext.ts
+++ frontend/src/context/AuthContext.ts
@@ -1,19 +1,19 @@
-import React from 'react';
+// import React from 'react';
 
-type AuthContextType = {
-  isAuthenticated: boolean;
-  token: string | null;
-  role: string | null;
-  login: (username: string, password: string) => Promise<void>;
-  logout: () => void;
-};
+// type AuthContextType = {
+//   isAuthenticated: boolean;
+//   token: string | null;
+//   role: string | null;
+//   login: (username: string, password: string) => Promise<void>;
+//   logout: () => void;
+// };
 
-const AuthContext = React.createContext<AuthContextType>({
-  isAuthenticated: false,
-  token: null,
-  role: null,
-  login: async () => {},
-  logout: () => {},
-});
+// const AuthContext = React.createContext<AuthContextType>({
+//   isAuthenticated: false,
+//   token: null,
+//   role: null,
+//   login: async () => {},
+//   logout: () => {},
+// });
 
-export default AuthContext;
+// export default AuthContext;
